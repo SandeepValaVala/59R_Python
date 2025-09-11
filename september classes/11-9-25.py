5bfaf3cb2ed27077adf78978059f886171d836c3
SandeepValaVala <Sandeepvalavala143@gmail.com>
2025-09-11 12:45:35 +0530

    date wise

--- september classes/11-9-25.py
+++ september classes/11-9-25.py
@@ -1,1 +1,15 @@
 # variables and methods
+
+# Types of variables
+# 2 types of variables - Instance variables and class/static variables
+# Types of  methods
+#  3 Types of methods - Instance methods, class methods and static methods
+
+# Instance variable => 
+# class/static variables => you don't need object to say its value
+
+
+
+class Calc:
+    Company_name = "casio"
+    
